91a81e4362e1387c97914e1221045a83a10726e0
eladkeyshawn <elad.keyshawn@gmail.com>
2021-03-20 18:57:20 +0200

    organize

--- content-script.js
+++ content-script.js
@@ -1,3 +1,14 @@
+class ACTIONS {
+  static BOOKMARK = "bookmark";
+  static FAVORITE = "favorite";
+  static CHECKED = "checked";
+}
+function getDomain() {
+  const url = window.location.href;
+  const arr = url.split("?");
+  return arr[0];
+}
+
 window.onload = () => {
   const checkSVG = chrome.runtime.getURL("assets/v2/check.svg");
   const checkHollowSVG = chrome.runtime.getURL("assets/v2/check-hollow.svg");
@@ -8,14 +19,23 @@ window.onload = () => {
     "assets/v2/bookmark-hollow.svg"
   );
 
-  function getDomain() {
-    const url = window.location.href;
-    const arr = url.split("?");
-    return arr[0];
+  init();
+
+  async function toggleUrlStateKey(url, stateKey) {
+    const urlState = await getUrlState(url);
+
+    return new Promise((resolve) => {
+      urlState[stateKey] = !urlState[stateKey];
+
+      chrome.storage.sync.set({
+        [url]: urlState,
+      });
+
+      resolve(urlState);
+    });
   }
 
-
-  async function toggleUrlStateKey(url, stateKey){
+  async function getUrlState(url) {
     return new Promise((resolve) => {
       chrome.storage.sync.get(url, (data) => {
         const urlState = data[url] || {
@@ -24,14 +44,9 @@ window.onload = () => {
           bookmark: false,
         };
 
-        urlState[stateKey] = !urlState[stateKey];
-        chrome.storage.sync.set({
-          [url]: urlState,
-        });
-
         resolve(urlState);
       });
-    })
+    });
   }
 
   function init() {
@@ -40,13 +55,17 @@ window.onload = () => {
   }
 
   function getIcon({ action, isActive }) {
-    if (action === "favorite") return isActive ? heartSVG : heartHollowSVG;
-    else if (action === "checked") return isActive ? checkSVG : checkHollowSVG;
-    else if (action === "bookmark")
-      return isActive ? bookmarkSVG : bookmarkHollowSVG;
+    switch (action) {
+      case ACTIONS.CHECKED: return isActive ? checkSVG : checkHollowSVG;
+      case ACTIONS.FAVORITE: return isActive ? heartSVG : heartHollowSVG;
+      case ACTIONS.BOOKMARK: return isActive ? bookmarkSVG : bookmarkHollowSVG;
+    }
   }
 
-  function createActionNode({ title, action, iconClassName, linkUrl, link }, isActive) {
+  function createActionNode(
+    { title, action, iconClassName, linkUrl, link },
+    isActive
+  ) {
     const containerNode = document.createElement("span");
     containerNode.setAttribute("class", "gm-wrapper");
     containerNode.setAttribute("title", title);
@@ -63,100 +82,90 @@ window.onload = () => {
     return containerNode;
   }
 
-
-
-
-  function initGithubMarkerAddon() {
+  async function initGithubMarkerAddon() {
     const listElements = document.querySelectorAll(".Box-body li");
 
-    listElements.forEach((listElement) => {
+    for (const listElement of listElements) {
       const link = listElement.querySelector("a");
-      if (link) {
-        const linkUrl = link.href;
-        chrome.storage.sync.get(linkUrl, (data) => {
-            const urlState = data[linkUrl] || {};
-
-            const checkboxContainer = createActionNode({
-                title: "Link read",
-                action: "checked",
-                iconClassName: "gm-checkbox-icon",
-                linkUrl,
-                link,
-              },
-              urlState.checked);
-
-              const favoriteContainer = createActionNode({
-                title: "Favorite Link",
-                action: "favorite",
-                iconClassName: "gm-heart-icon",
-                linkUrl,
-                link,
-              }, urlState.favorite);
-
-              const bookmarkContainer = createActionNode({
-                title: "Bookmark Link",
-                action: "bookmark",
-                iconClassName: "gm-bookmark-icon",
-                linkUrl,
-                link,
-              }, urlState.bookmark);
-
-              const appContainer = document.createElement("span");
-              appContainer.setAttribute("class", "gm-app-container");
-
-              const container = document.createElement("span");
-              container.setAttribute("class", "gm-container");
-
-              container.appendChild(checkboxContainer);
-              container.appendChild(favoriteContainer);
-              container.appendChild(bookmarkContainer);
-
-              appContainer.appendChild(container);
-
-              listElement.insertBefore(appContainer, listElement.childNodes[0]);
-        })
-
-
+      if (!link) {
+        continue;
       }
-    });
+
+      const linkUrl = link.href;
+      const urlState = await getUrlState(linkUrl);
+
+      const checkboxContainer = createActionNode(
+        {
+          title: "Link read",
+          action: "checked",
+          iconClassName: "gm-checkbox-icon",
+          linkUrl,
+          link,
+        },
+        urlState.checked
+      );
+
+      const favoriteContainer = createActionNode(
+        {
+          title: "Favorite Link",
+          action: "favorite",
+          iconClassName: "gm-heart-icon",
+          linkUrl,
+          link,
+        },
+        urlState.favorite
+      );
+      const bookmarkContainer = createActionNode(
+        {
+          title: "Bookmark Link",
+          action: "bookmark",
+          iconClassName: "gm-bookmark-icon",
+          linkUrl,
+          link,
+        },
+        urlState.bookmark
+      );
+
+      const appContainer = document.createElement("span");
+      appContainer.setAttribute("class", "gm-app-container");
+      const container = document.createElement("span");
+      container.setAttribute("class", "gm-container");
+      container.appendChild(checkboxContainer);
+      container.appendChild(favoriteContainer);
+      container.appendChild(bookmarkContainer);
+      appContainer.appendChild(container);
+      listElement.insertBefore(appContainer, listElement.childNodes[0]);
+    }
   }
-  function saveAllLinks(){
-    const listElements = document.querySelectorAll(".Box-body li");
-    var i = 0;
-    listElements.forEach((listElement) => {
-        setTimeout(() => {
-            const link = listElement.querySelector("a");
-            updateUrlState(link, 'checked');
-        },100 * i++)
-    });
-  }
+
+  const iconCssClassToAction = {
+    "gm-checkbox-icon": ACTIONS.CHECKED,
+    "gm-heart-icon": ACTIONS.FAVORITE,
+    "gm-bookmark-icon": ACTIONS.BOOKMARK,
+  };
+
+  const actionToLinkCssClass = {
+    [ACTIONS.CHECKED]: "gm-checked",
+    [ACTIONS.FAVORITE]: "gm-favorite",
+    [ACTIONS.BOOKMARK]: "gm-bookmark",
+  };
 
   async function onClick(event) {
     const url = event.target.getAttribute("data-url");
-
     if (!url) return;
 
     const parentLINode = event.target.closest("li");
     const linkNode = parentLINode.querySelector("a");
 
     const { className } = event.target;
-    let key;
+    const action = iconCssClassToAction[className];
 
-    if (className === "gm-checkbox-icon") {
-      key = "checked";
-      linkNode.classList.toggle("gm-checked");
-    } else if (className === "gm-heart-icon") {
-      key = "favorite";
-      linkNode.classList.toggle("gm-favorite");
-    } else if (className === "gm-bookmark-icon") {
-      key = "bookmark";
-      linkNode.classList.toggle("gm-bookmark");
+    try {
+      const urlState = await toggleUrlStateKey(url, action);
+      event.target.src = getIcon({ action, isActive: urlState[action] });
+      linkNode.classList.toggle(actionToLinkCssClass[action]);
+    } catch (err) {
+      alert(err);
     }
-
-    const newStatus = await toggleUrlStateKey(url, key);
-    event.target.src = getIcon({ action: key, isActive: newStatus });
   }
-
-  init();
-
 };
